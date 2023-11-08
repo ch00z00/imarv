ed39726c97cc060295b102a94a788446260fa0c8
ch00z00 <choozoo10@gmail.com>
2023-11-08 17:16:28 +0900

    [add] Added links to post and about pages.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <>
+      <div className="flex min-h-screen flex-col justify-start bg-yellow-200 text-black -200">
+        <div className="text-[min(14vw,170px)] w-[min(75vw)] font-ogg pt-[min(18vh)] pl-[min(4vw)] leading-none tracking-wider">About</div>
+      </div>
+    </>
+  );
+}
