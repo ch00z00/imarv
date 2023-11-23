4147e772fbdf7339235672213242bd5503fe5849
ch00z00 <choozoo10@gmail.com>
2023-11-24 04:31:12 +0900

    [fix] Fixed image quality and add some contents of post-01.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="flex min-h-screen flex-col justify-center items-center w-screen bg-black-200 text-yellow-200 px-[min(12vw)]">
-        <div className="text-[min(14vw,170px)] font-ogg tracking-wider">imarv</div>
+      <div className="flex min-h-screen flex-col justify-center items-center w-screen bg-black-100 text-yellow-200 px-[min(12vw)] text-[min(3vw)] font-zen-old-mincho">
         <button
           type="button"
           className="relative inline-flex items-center justify-start overflow-hidden"
           >
-            <Link href="/posts/01" legacyBehavior>Post-01</Link>
+            <Link href="/posts/01" legacyBehavior>ラスト3分怒涛の伏線回収・衝撃展開</Link>
         </button>
         <button
           type="button"
